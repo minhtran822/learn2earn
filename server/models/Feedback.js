37b93291b86224d4d6b95b0323d15b7d56b820b2
Minh Tran <trnmi822@student.otago.ac.nz>
2022-05-08 00:50:36 +1200

    Add Feedback schema model

---
+++ server/models/Feedback.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const FeedbackSchema = new Schema({
+
+    userID : {
+        type: Schema.Types.ObjectId, 
+        ref: "User"
+    },
+
+    feedback: {
+        type: String,
+        required: true
+    },
+});
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+module.exports = Feedback;
